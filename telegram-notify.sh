29fc87bde05c8fa82e9a1926fd9f5e7a8fbf7337
spaww <the.spaww@gmail.com>
2016-08-14 11:53:46 -0300

    Add telegram script

---
+++ telegram-notify.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+#Autor: Adail Horst (the.spaww@gmail.com)
+#Site: http://www.spinola.net.br 
+### Script de integracao com o Telegram ### 
+#set -x
+#SCRIPT_PATH=`pwd`;
+ENTER="
+";
+USERID="$1";
+#KEY=$(cat $SCRIPT_PATH/botinfo.txt);
+KEY="CHAVE_DO_MEU_BOT";
+
+TIMEOUT="5";
+TEXT=$(echo -e "*$2* $ENTER$3" |  sed 's/PROBLEM/INCIDENTE/g');
+
+URL="https://api.telegram.org/bot$KEY/sendMessage"
+
+echo "DATA - TIPO - USERID - TIMEOUT - URL" >> /tmp/telegram-notify.log
+echo "$(date +%s) - REQUEST - $USERID $TIMEOUT $URL " >> /tmp/telegram-notify.log
+RESPONSE=`curl -s --max-time $TIMEOUT -d "chat_id=$USERID&disable_web_page_preview=1&parse_mode=markdown&text=$TEXT" $URL`;
+echo "$(date +%s) - RESPONSE - $RESPONSE" >> /tmp/telegram-notify.log
+
+chown zabbix: /tmp/telegram-notify.log
